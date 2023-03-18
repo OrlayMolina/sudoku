59467599e24d4a9555f7b950ce2eecfd3b80c792
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-03-18 16:37:21 -0500

    Se crean métodos de apoyo en la generación de la solución para el sudoku.

--- src/main/java/modelo/Sudoku.java
+++ src/main/java/modelo/Sudoku.java
@@ -1,11 +1,16 @@
 package modelo;
 
+/**
+ * @author Orlay Andrés Molina Gómez
+ * version 1.0
+ */
+
 public class Sudoku {
 
     private int sudoku[][];
 
     public Sudoku(){
-        int sudo[][] = {
+        int[][] sudo = {
                 {0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -18,6 +23,34 @@ public class Sudoku {
         };
         sudoku=sudo;
     }
+
+    /**
+     * Función generarSudoku recibe por parámetro una matriz de 9x9  y subdivide la matriz en 9 bloques 3x3,
+     * en dónde generará tres números aleatorios con la función random() en el rango de 1 a 9 para cada
+     * sub-bloque de 3x3 que no puede ser el mismo número, adicionalmente comprobará en la matriz macro de
+     * 9x9 que no se repita números en la misma fila y columna.
+     * @param sudoku
+     * @param fila
+     * @param columna
+     * @return
+     */
+    public static int[][] generarSudoku(int[][] sudoku,int fila, int columna){
+        if(fila < 2){
+
+        }
+
+        return sudoku;
+    }
+
+    /**
+     * Función instruccionInicial() usada para explicar al usuario la finalidad del programa.
+     */
+    public static void instruccionInicial(){
+        System.out.println("\n                                      SUDOKU\n");
+        System.out.println("El juego de SUDOKU presentado en este proyecto se generará automaticamente, luego");
+        System.out.print("se completará por backtraking e informará al usuario si este tenia solución.");
+    }
+
 
     public int[][] getSudoku() {
         return sudoku;
---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,12 @@
+package principal;
+
+import static modelo.Sudoku.instruccionInicial;
+
+public class Principal {
+
+    public static void main( String[] args ){
+
+        instruccionInicial();
+
+    }
+}
