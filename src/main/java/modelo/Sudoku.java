3c21df27cc0131a1ad9cb99a070cb6a3b0f95dad
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-03-18 15:33:59 -0500

    Se crea el proyecto para la creación de un sudoku de 9x9 utilizando backtraking.

---
+++ src/main/java/modelo/Sudoku.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class Sudoku {
+
+    private int sudoku[][];
+
+    public Sudoku(){
+        int sudo[][] = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
+        sudoku=sudo;
+    }
+
+    public int[][] getSudoku() {
+        return sudoku;
+    }
+
+    public void setSudoku(int[][] sudoku) {
+        this.sudoku = sudoku;
+    }
+}
