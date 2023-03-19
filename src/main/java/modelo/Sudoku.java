73519b9634d4dfe13bce6697fd69585c63de9535
OrlayMolina <111409267+OrlayMolina@users.noreply.github.com>
2023-03-19 12:31:38 -0500

    Código backtracking

--- src/main/java/modelo/Sudoku.java
+++ src/main/java/modelo/Sudoku.java
@@ -7,33 +7,92 @@ package modelo;
 
 public class Sudoku {
 
-    private int sudoku[][];
+    private static int[][] sudoku;
 
-    public Sudoku(int[][] sudoku) {
-        this.sudoku = sudoku;
+    public Sudoku() {
+        int[][] sudo = {
+                {0, 6, 0, 1, 0, 4, 0, 5, 0},
+                {0, 0, 8, 3, 0, 5, 6, 0, 0},
+                {2, 0, 0, 0, 0, 0, 0, 0, 1},
+                {8, 0, 0, 4, 0, 7, 0, 0, 6},
+                {0, 0, 6, 0, 0, 0, 3, 0, 0},
+                {7, 0, 0, 9, 0, 1, 0, 0, 4},
+                {5, 0, 0, 0, 0, 0, 0, 0, 2},
+                {0, 0, 7, 2, 0, 6, 9, 0, 0},
+                {0, 4, 0, 5, 0, 8, 0, 7, 0},
+        };
+        this.sudoku = sudo;
     }
 
-    /**
-     * Función generarSudoku recibe por parámetro una matriz de 9x9  y subdivide la matriz en 9 bloques 3x3,
-     * en dónde generará tres números aleatorios con la función random() en el rango de 1 a 9 para cada
-     * sub-bloque de 3x3 que no puede ser el mismo número, adicionalmente comprobará en la matriz macro de
-     * 9x9 que no se repita números en la misma fila y columna.
-     * @param sudoku
-     * @param fila
-     * @param columna
-     * @return
-     */
-    public static int[][] generarSudoku(int[][] sudoku,int fila, int columna){
-        if(fila < 2){
-
+    public static boolean resolverSudoku() {
+        for (int fila = 0; fila < sudoku.length; fila++) {
+            for (int columna = 0; columna < sudoku[0].length; columna++) {
+                if (sudoku[fila][columna] == 0) {
+                    for (int valor = 1; valor <= 9; valor++) {
+                        if (validarFila(fila, valor,columna) && validarColumna(columna, valor, fila) && validarCuadrante(fila, columna, valor)) {
+                            sudoku[fila][columna] = valor;
+                            if (resolverSudoku()) {
+                                return true;
+                            }
+                            sudoku[fila][columna] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
         }
-
-        return sudoku;
+        return true;
     }
 
-    /**
-     * Función instruccionInicial() usada para explicar al usuario la finalidad del programa.
-     */
+
+    public static boolean validarCuadrante(int fila, int columna, int valor) {
+        int posI = subCuadranteActual(fila);
+        int posJ = subCuadranteActual(columna);
+
+        for (int i = posI - 3; i < posI; i++) {
+            for (int j = posJ - 3; j < posJ; j++) {
+                if (sudoku[i][j] == valor) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    public static int subCuadranteActual(int pos) {
+        if (pos <= 2) {
+            return 3;
+        } else if (pos <= 5) {
+            return 6;
+        } else {
+            return 9;
+        }
+    }
+
+    public static boolean validarFila(int fila, int valor, int columna) {
+        if (columna == sudoku[fila].length) {
+            return true;
+        } if (sudoku[fila][columna] == valor) {
+            return false;
+        }
+        return validarFila(fila, valor, columna + 1);
+    }
+
+
+    public static boolean validarColumna(int columna, int valor, int fila) {
+        if (fila == sudoku.length) {
+            return true;
+        } if (sudoku[fila][columna] == valor) {
+            return false;
+        }
+        return validarColumna(columna, valor, fila + 1);
+    }
+
+
+            /**
+             * Función instruccionInicial() usada para explicar al usuario la finalidad del programa.
+             */
     public static void instruccionInicial(){
         System.out.println("\n                                      SUDOKU\n");
         System.out.println("El juego de SUDOKU presentado en este proyecto se generará automáticamente, luego");
@@ -42,23 +101,44 @@ public class Sudoku {
 
     /**
      * Función recorrerSudoku() empleada para pasar por cada uno de los componenetes del SUDOKU con recursividad.
-     * @param matriz
      * @param fila
      * @param columna
      */
-    public static void recorrerSudoku(int[][] matriz, int fila, int columna){
-        if(fila == matriz.length || columna == matriz.length){
+    public static void recorrerSudoku (int fila, int columna){
+        if(fila == sudoku.length || columna == sudoku.length){
             System.out.println("\n      --- SUDOKU RECORRIDO ---");
         } else {
-            System.out.print(" "+matriz[fila][columna]+" |");
-            if(columna == matriz.length-1){
+            System.out.print(" "+sudoku[fila][columna]+" |");
+            if(columna == sudoku.length-1){
                 fila++;
                 columna = 0;
                 System.out.println("");
             }else{
                 columna++;
             }
-            recorrerSudoku(matriz, fila, columna);
+            recorrerSudoku(fila, columna);
+        }
+    }
+
+    /**
+     * Función recorrerSudokuSolucion() emplea dentro de sí misma la función resolverSudoku() para buscar la solución al SUDOKU planteado.
+     * @param fila
+     * @param columna
+     */
+    public static void recorrerSudokuSolucion (int fila, int columna){
+        resolverSudoku();
+        if(fila == sudoku.length || columna == sudoku.length){
+            System.out.println("");
+        } else {
+            System.out.print(" "+sudoku[fila][columna]+" |");
+            if(columna == sudoku.length-1){
+                fila++;
+                columna = 0;
+                System.out.println("");
+            }else{
+                columna++;
+            }
+            recorrerSudokuSolucion(fila, columna);
         }
     }
 
--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -2,34 +2,22 @@ package principal;
 
 import modelo.Sudoku;
 
-import static modelo.Sudoku.instruccionInicial;
-import static modelo.Sudoku.recorrerSudoku;
+import static modelo.Sudoku.*;
 
 public class Principal {
 
-    static int[][] sudo = {
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0},
-    };
 
     public static void main( String[] args ){
 
-        Sudoku sudoku = new Sudoku(sudo);
+        Sudoku sudoku = new Sudoku();
         instruccionInicial();
-        recorrerSudoku(sudo, 0, 0);
+        recorrerSudoku(0,0);
         //generarSudoku(sudo, 0, 0);
-        System.out.println("\nJuego de Sudoku generado automáticamente.");
-        //recorrerSudoku(sudo, 0, 0);
-        System.out.println("\nGenerando solución ... Espere por favor\n");
+        System.out.println("\nNúmeros de Sudoku generados automáticamente.");
+        //recorrerSudoku();
+        System.out.println("\nGenerando solución al SUDOKU ... Espere por favor\n");
         System.out.println("      --- SOLUCIÓN HALLADA ---\n");
-        //recorrerSudoku(sudo, 0, 0);
+        recorrerSudokuSolucion( 0, 0);
         System.out.println("      --- FIN DEL PROGRAMA ---\n");
 
     }
